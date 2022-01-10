39d6c10397f60136225cf2134ff033e02a7ce72d
“William <Williamartins02@hotmail.com>
2022-01-10 20:47:21 -0300

    componente HOME criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,11 +30,13 @@ import {MatCardModule} from '@angular/material/card';
 
 //component do projeto.
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
