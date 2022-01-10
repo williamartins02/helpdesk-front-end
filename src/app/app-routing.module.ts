a496319b35af9dc9ad5d6eefff055c30b068ec7c
“William <Williamartins02@hotmail.com>
2022-01-10 20:57:30 -0300

    ROTA para componente HOME

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -5,7 +6,9 @@ import { RouterModule, Routes } from '@angular/router';
 //Fica toda roda do projeto para ser renderizado.
 const routes: Routes = [
   {
-    path: '', component: NavComponent
+    path: '', component: NavComponent, children: [
+      { path: 'home', component: HomeComponent }
+    ]
   }
 ];
 
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,3 +1,4 @@
+import { Router, Routes } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
+  //Metodo reinicia componentes que precisa ser iniciado
   ngOnInit(): void {
+   this.router.navigate(['home'])
   }
 
 }
