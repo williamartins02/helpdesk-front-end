20d8a532ffe7e925562ddbcb0170e3c93feac3d5
“William <Williamartins02@hotmail.com>
2022-01-11 14:43:26 -0300

    Rota para TECNICO-LIST

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 import { NgModule } from '@angular/core';
@@ -8,7 +9,8 @@ const routes: Routes = [
   {
     path: '', component: NavComponent, 
     children: [
-      { path: 'home', component: HomeComponent }
+      { path: 'home', component: HomeComponent },
+      {path: 'tecnicos', component: TecnicoListComponent}
     ]
   }
 ];
