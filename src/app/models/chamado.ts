f12165537521336373d29446a8426331067a3bed
“William <Williamartins02@hotmail.com>
2022-01-19 16:34:36 -0300

    Interface chamado criado

---
+++ src/app/models/chamado.ts
@@ -0,0 +1,12 @@
+export interface Chamado{
+    id?:                 any;
+    dataAbertura?:       string;
+    dataFechamento?:     string;
+    prioridade:          string;
+    status:              string;
+    titulo:              string;
+    descricao:           string;
+    tecnico:             any;
+    nomeCliente:         string;
+    nomeTecnico:         string;
+}
