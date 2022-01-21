a9a663f0615c61566277d2d2a470134ddf6b7ba2
“William <Williamartins02@hotmail.com>
2022-01-20 23:28:31 -0300

    Serviço para criar chamado implementado.

--- src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,3 +1,13 @@
+import { Router } from '@angular/router';
+import { Chamado } from './../../../models/chamado';
+
+import { throwError } from "rxjs";
+import { ToastrService } from "ngx-toastr";
+import { ChamadoService } from "src/app/services/chamado.service";
+import { TecnicoService } from "./../../../services/tecnico.service";
+import { ClienteService } from "./../../../services/cliente.service";
+import { Tecnico } from "./../../../models/tecnico";
+import { Cliente } from "./../../../models/cliente";
 import { Component, OnInit } from "@angular/core";
 import { FormControl, Validators } from "@angular/forms";
 
@@ -8,22 +18,78 @@ import { FormControl, Validators } from "@angular/forms";
 })
 export class ChamadoCreateComponent implements OnInit {
 
-  prioridade: FormControl = new FormControl(null, [Validators.required])
-  status:     FormControl = new FormControl(null, [Validators.required])
-  titulo:     FormControl = new FormControl(null, [Validators.required])
-  descricao:  FormControl = new FormControl(null, [Validators.required])
-  tecnico:    FormControl = new FormControl(null, [Validators.required])
-  cliente:    FormControl = new FormControl(null, [Validators.required])
+  chamado: Chamado = {
+    prioridade: '',
+    status:     '',
+    titulo:     '',
+    observacoes:  '',
+    tecnico:    '',
+    cliente:    '',
+    nomeCliente:'',
+    nomeTecnico:'',
+  }
+  clientes: Cliente[] = [];
+  tecnicos: Tecnico[] = [];
 
-  constructor() {}
 
-  ngOnInit(): void {}
+  prioridade: FormControl = new FormControl(null, [Validators.required]);
+  status: FormControl = new FormControl(null, [Validators.required]);
+  titulo: FormControl = new FormControl(null, [Validators.required]);
+  observacoes: FormControl = new FormControl(null, [Validators.required]);
+  tecnico: FormControl = new FormControl(null, [Validators.required]);
+  cliente: FormControl = new FormControl(null, [Validators.required]);
+
+  constructor(
+    private chamadoService: ChamadoService,
+    private clienteService: ClienteService,
+    private tecnicoService: TecnicoService,
+    private toast: ToastrService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.findaAllClientes();
+    this.findAllTecnico();
+  }
+
+  create(): void{
+     this.chamadoService.create(this.chamado).subscribe((resposta) =>{
+      this.toast.success("Chamado criando com sucesso", "Novo chamado");
+      this.router.navigate(['chamados']);
+    },(error) => {
+      this.toast.error("Ao adicionar um chamado", "ERROR");
+      return throwError(error.error.error);
+    });
+  }
+
+  findaAllClientes(): void {
+    this.clienteService.findAll().subscribe((resposta) => {
+        this.clientes = resposta;
+      },(error) => {
+        this.toast.error("Ao carregar a lista técnico", "ERROR");
+        return throwError(error);
+      });
+  }
+
+  findAllTecnico(): void {
+    this.tecnicoService.findAll().subscribe(
+      (resposta) => {
+        this.tecnicos = resposta;
+      },(error) => {
+        this.toast.error("Ao carregar a lista técnico", "ERROR");
+        return throwError(error);
+      }
+    );
+  }
 
   validaCampos(): boolean {
     return (
-      this.prioridade.valid && this.status.valid &&
-      this.titulo.valid && this.descricao.valid &&
-      this.tecnico && this.cliente.valid
+      this.prioridade.valid &&
+      this.status.valid &&
+      this.titulo.valid &&
+      this.observacoes.valid &&
+      this.tecnico &&
+      this.cliente.valid
     );
   }
 }
--- src/app/services/chamado.service.ts
+++ src/app/services/chamado.service.ts
@@ -1,18 +1,34 @@
 import { API_CONFIG } from "./../config/api.config";
 import { Chamado } from "./../models/chamado";
-import { Observable } from "rxjs";
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable, pipe, Subject, tap } from 'rxjs';
 
 @Injectable({
   providedIn: "root",
 })
 export class ChamadoService {
-  constructor(
-    private http: HttpClient
-    ) {}
+
+  constructor( private http: HttpClient) {}
+
+   private _refresh$ = new Subject<void>();
+
+    get refresh$() {
+      return this._refresh$;
+    }
+  
 
   findAll(): Observable<Chamado[]> {
     return this.http.get<Chamado[]>(`${API_CONFIG.baseUrl}/chamados`);
   }
+
+  create(chamado: Chamado): Observable<Chamado> {
+    return this.http.post<Chamado>(`${API_CONFIG.baseUrl}/chamados`, chamado)
+      .pipe(
+        tap(() => {
+          this._refresh$.next();
+        })
+      )
+  }
+
 }
