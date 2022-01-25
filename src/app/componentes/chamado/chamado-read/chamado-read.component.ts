5224edde7de6c33eea5b614ef160a4a1ed5cf1ed
“William <Williamartins02@hotmail.com>
2022-01-25 15:43:55 -0300

    Component CHAMADO-READ finalizado

--- src/app/componentes/chamado/chamado-read/chamado-read.component.ts
+++ src/app/componentes/chamado/chamado-read/chamado-read.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Chamado } from './../../../models/chamado';
+
+import { throwError } from "rxjs";
+import { ToastrService } from "ngx-toastr";
+import { ChamadoService } from "src/app/services/chamado.service";
+import { ChamadoUpdateComponent } from 'src/app/components/chamado/chamado-update/chamado-update.component';
 
 @Component({
   selector: 'app-chamado-read',
@@ -6,10 +14,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chamado-read.component.css']
 })
 export class ChamadoReadComponent implements OnInit {
+  chamado: Chamado = {
+    prioridade:  '',
+    status:      '',
+    titulo:      '',
+    observacoes: '',
+    tecnico:     '',
+    cliente:     '',
+    nomeCliente: '',
+    nomeTecnico: '',
+  }
+ 
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: {id: Number},
+
+    private chamadoService: ChamadoService,
+    private toast: ToastrService,
+    private route: ActivatedRoute,
+
+    public dialogRef: MatDialogRef<ChamadoUpdateComponent>,
+  ) {}
 
   ngOnInit(): void {
+    this.chamado.id = this.route.snapshot.paramMap.get('id');//passando id para o editar via url
+    this.findById();
+  }
+  
+  findById(): void{
+    this.chamadoService.findById(this.data.id).subscribe((resposta) =>{
+      this.chamado = resposta;
+    },(error) => {
+      this.toast.error("ao chamar Tecnico ID", "ERROR");
+      return throwError(error.error.error);
+    })
   }
 
+  /**Retornando status como string*/
+  retornaStatus(status: any): string {
+    if(status == '0') {
+      return 'ABERTO'
+    } else if(status == '1') {
+      return 'EM ANDAMENTO'
+    } else {
+      return 'ENCERRADO'
+    }
+  }
+
+  retornaPrioridade(prioridade: any): string {
+    if(prioridade == '0') {
+      return 'BAIXA'
+    } else if(prioridade == '1') {
+      return 'MÉDIA'
+    } else {
+      return 'ALTA'
+    }
+  }
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
